6193775e1949b9fc48a0ebf34fcaeba35e0faa96
Markus Herhoffer <markus.herhoffer@exentra.de>
2025-11-30 16:27:28 +0100

    Remove unused resources, health checks, and templates
    - Deleted `GreetingResource`, `GreetingResourceTest`, and `GreetingResourceIT`.
    - Removed `MyLivenessCheck` and associated health check logic.
    - Eliminated `SomePage` resource and its template functionality.

--- src/test/java/net/herhoffer/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package net.herhoffer;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
